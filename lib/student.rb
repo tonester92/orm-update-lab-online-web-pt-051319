0592857154ba010518302ef0272b97e05286b66a
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-22 00:33:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,16 +21,16 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+
   def self.drop_table
     sql = <<-SQL
       DROP TABLE students
     SQL
     
     DB[:conn].execute(sql)
-  end
-  
-  def self.create(name, grade)
-    student = self.new(name, grade)
-    student.save
-    student
   end
