aca08ad0203fe75ae9bf69bcab0c6e33ed33d97c
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-22 00:31:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,19 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :id, :name, :grade 
   
-  def
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
